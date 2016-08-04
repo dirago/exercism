600adfe9769bdc4c17973fd1cd739eea49190834
dirago <diragor@gmail.com>
2016-08-04 23:53:51 +0200

    hamming done

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,5 +1,17 @@
 var Hamming = function() {};
 
-function compute(a, b) {
-    
+Hamming.prototype.compute = function(a, b) {
+    var result = 0;
+    if (a.length !== b.length) {
+        throw new Error('DNA strands must be of equal length.');
+    } else {
+        for (var i = 0; i < a.length; i++) {
+            if (a[i] !== b[i]) {
+            result++;
+            }
+        }
+        return result
+    }
 }
+
+module.exports = Hamming;
