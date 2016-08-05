44f711acc02def5cf3a2528a2d134e687ab6c5ae
dirago <diragor@gmail.com>
2016-08-05 10:22:31 +0200

    bob done

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,15 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+    if (input.trim() === "") {
+        return 'Fine. Be that way!';
+    } else if (input.toUpperCase() === input && input.toLowerCase() !== input) {
+        return 'Whoa, chill out!';
+    } else if (input.charAt(input.length - 1) === "?" ) {
+        return 'Sure.';
+    }  else {
+        return 'Whatever.';
+    }
 };
 
 module.exports = Bob;
