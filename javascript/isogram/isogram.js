0aae9187d196c3b0ea22e972e3d56306679db8ea
dirago <diragor@gmail.com>
2016-08-15 00:55:59 +0200

    isogram finished

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,7 +1,19 @@
-var Isogram = function() {};
+var Isogram = function(str) {
+    this.str = str;
+};
 
 Isogram.prototype.isIsogram = function() {
-    
-}
+    str = this.str.toLowerCase();
+    regex = /[a-zÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ]/gi
+    obj = {};
+    for (var i = 0; i < str.length; i++) {
+        if (obj.hasOwnProperty(str[i]) && str[i].match(regex)) {
+            return false;
+        } else {
+            obj[str[i]] = 1;
+        }
+    }
+    return true;
+};
 
 module.exports = Isogram;
